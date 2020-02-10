63b0fba33d956652ac025e9c297234d67fc5daf7
github username <github email address>
2020-02-10 16:51:23 -0500

    started setting up score submit form

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,8 +1,23 @@
 class GameSessionsController < ApplicationController
+  
 
   def index
     gameSessions = GameSession.all
     render json: gameSessions
   end
+
+  def new
+  end
+
+  def create
+    gameSession = GameSession.find(session_params)
+  end
+
+  private
+
+  def session_params
+    params.require(:gameSession).permit(:name)
+  end
+
 
 end
