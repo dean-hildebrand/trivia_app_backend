ad0b03a217d87aad94b0374f751355b93bb23cf2
github username <github email address>
2020-02-12 14:18:29 -0500

    can sort scores highest to lowest on back end

--- app/models/game_session.rb
+++ app/models/game_session.rb
@@ -1,3 +1,13 @@
 class GameSession < ApplicationRecord
     
+    def self.sort_scores
+        GameSession.order(score: :desc)
+    end
+
+    def self.get_all_scores
+        sort_scores.pluck(:score)
+    end
+        
+
 end
+    
